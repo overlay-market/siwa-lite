3d133bc9bcb25be91b0a002550bec66ae15e8475
realism <meow@overlay.foundation>
2024-02-11 03:24:02 -0700

    setting up basic node_monkes feed

---
+++ feeds/node_monkes.py
@@ -0,0 +1,24 @@
+from feeds.data_feed import DataFeed
+from collections import deque
+
+from apis.unisat import UnisatAPI
+
+
+class NodeMonkes(DataFeed):
+    NAME = 'node_monkes'
+    HEARTBEAT = 180
+    DATAPOINT_DEQUE = deque([], maxlen=100)
+
+    COLLECTION_ID = 'nodemonkes'
+
+    @classmethod
+    def process_source_data_into_siwa_datapoint(cls):
+        '''
+            Process data from multiple sources
+        '''
+        node_monkes_data = UnisatAPI().get_collection_stats(cls.COLLECTION_ID).json()["data"]
+        floor_price = node_monkes_data["floor_price"]
+
+    @classmethod
+    def create_new_data_point(cls):
+        return cls.process_source_data_into_siwa_datapoint()
