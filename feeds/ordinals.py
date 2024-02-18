e8d242121d89ee39395ce5a977bc591299843c66
realism <meow@overlay.foundation>
2024-02-17 19:07:23 -0700

    cleanup Ordinals base class

--- feeds/ordinals.py
+++ feeds/ordinals.py
@@ -1,8 +1,10 @@
 from feeds.data_feed import DataFeed
 from collections import deque 
+import numpy as np
 
 #from apis.unisat import UnisatAPI   
 from apis.magic_eden import MagicEdenAPI
+from apis.OKX import OKXAPI
 
 class OrdinalsFeed(DataFeed):
     HEARTBEAT = 180
@@ -19,14 +21,16 @@ class OrdinalsFeed(DataFeed):
             Process data from multiple sources
         '''
         #node_monkes_data = UnisatAPI().get_collection_stats(cls.COLLECTION_ID).json()["data"]
-        #breakpoint()
-        #floor_price = node_monkes_data[cls.FLOOR_PX]
-        me_floor = MagicEdenAPI().get_floor_price(cls.COLLECTION_ID)
-        print(me_floor)
+        #unisat_floor_price = node_monkes_data[cls.FLOOR_PX]
+
+        me_floor = MagicEdenAPI().get_floor_price(cls.MAGIC_EDEN_COLLECTION_ID)
+        okx_floor = OKXAPI().get_floor_price(cls.OKX_COLLECTION_ID)
+        floor = np.median([me_floor, okx_floor])
+        print(cls.NAME, me_floor, okx_floor, floor)   
         if cls.DATAPOINT_DEQUE:
-            if me_floor is None or me_floor == 0 or me_floor/cls.DATAPOINT_DEQUE[-1] > 1.2 or me_floor/cls.DATAPOINT_DEQUE[-1] < 0.8:
+            if floor is None or floor == 0 or floor/cls.DATAPOINT_DEQUE[-1] > 1.2 or floor/cls.DATAPOINT_DEQUE[-1] < 0.8:
                 return cls.DATAPOINT_DEQUE[-1]
-        return me_floor
+        return floor
     
     @classmethod
     def create_new_data_point(cls):
