19c3b56b6622833b6f2fb646b284b4d06ae48b26
realism <meow@overlay.foundation>
2024-02-17 19:07:00 -0700

    adding bitcoin frogs feed

--- feeds/bitcoin_frogs.py
+++ feeds/bitcoin_frogs.py
@@ -2,9 +2,10 @@ from feeds.data_feed import DataFeed
 from feeds.ordinals import OrdinalsFeed
 
 class BitcoinFrogs(OrdinalsFeed):
+    ID = 99
     NAME = 'bitcoin_frogs'
-    COLLECTION_ID = 'bitcoin-frogs'
-
+    MAGIC_EDEN_COLLECTION_ID = 'bitcoin-frogs'
+    OKX_COLLECTION_ID = 'bitcoin-frogs'
 
 # def main():
 #     BitcoinFrogs.process_source_data_into_siwa_datapoint()
