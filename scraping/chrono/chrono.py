1e3519d4e3a43fbb9697759c9450c7173599847b
Roman <94143966+Carti23@users.noreply.github.com>
2024-03-01 12:51:25 +0200

    added (scraping/chrono): replaced prints to loggings

--- scraping/chrono/chrono.py
+++ scraping/chrono/chrono.py
@@ -1,3 +1,4 @@
+import logging
 from typing import List
 import requests
 from bs4 import BeautifulSoup
@@ -5,13 +6,14 @@ import pandas as pd
 
 
 class Chrono24Scraper:
-    User_Agent: str = (
-        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3"
-    )
+    User_Agent: str = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3"
 
     def __init__(self) -> None:
-        self.headers: dict = {"User-Agent": self.User_Agent}
+        self.headers: dict = {
+            "User-Agent": self.User_Agent
+        }
         self.data: List[str] = []
+        logging.basicConfig(level=logging.INFO)
 
     def scrape_names_from_page(self, url: str) -> None:
         response = requests.get(url, headers=self.headers)
@@ -24,33 +26,31 @@ class Chrono24Scraper:
             for i in range(len(titles)):
                 title = titles[i].text.strip()
                 price = prices[i].next_sibling.strip()
-                print(f"Name: {title}, Price: {price}$")
+                logging.info(f"Name: {title}, Price: {price}$")
         else:
-            print("Failed to retrieve the webpage. Status code:", response.status_code)
+            logging.error("Failed to retrieve the webpage. Status code: %d", response.status_code)
 
     def scrape_all_pages(self, base_url: str, start_page: int = 1) -> None:
         page_number = start_page
-        while page_number <= 50:
+        while True:
             url = base_url.format(page_number)
             try:
                 response = requests.get(url, headers=self.headers)
                 if response.status_code == 200:
-                    print(f"Scraping page {page_number}")
+                    logging.info(f"Scraping page {page_number}")
                     self.scrape_names_from_page(url)
                     page_number += 1
                 else:
-                    print(
-                        "Failed to retrieve the webpage. Status code:",
-                        response.status_code,
-                    )
+                    logging.error("Failed to retrieve the webpage. Status code: %d", response.status_code)
                     break
             except requests.exceptions.RequestException as e:
-                print("An error occurred while making the request:", e)
+                logging.error("An error occurred while making the request: %s", e)
                 break
 
     def save_to_csv(self, filename: str = "chrono24_data.csv") -> None:
         df = pd.DataFrame(self.data, columns=["Name", "Price"])
         df.to_csv(filename, index=False)
+        logging.info(f"Data saved to {filename}")
 
 
 if __name__ == "__main__":
